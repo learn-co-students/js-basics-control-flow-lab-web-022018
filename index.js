239d50a07dc589737dc0bfd7bfd0d31aa5f467fb
sinclairstudy <sinclairstudy@gmail.com>
2018-03-31 15:21:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  switch (true) {
+    case feet <= 400:
+      return 'This one is on me!';
+    case feet > 2000 && feet < 2500:
+      return 'I will gladly take your thirty bucks.';
+    default:
+      return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(generosity){
+  switch (generosity) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
