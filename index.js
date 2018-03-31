4dea1fed00959ae01610d9d2cb2e4f76b31de492
lidianvilazio <lidianvilazio@hotmail.com>
2018-03-30 23:15:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function scuberGreetingForFeet(n){
+  if( n <= 400) {
+    return 'This one is on me!';
+  } else if (n >= 2500) {
+    return 'No can do.';
+  } else if (n >= 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
+}
+
+function switchOnCharmFromTip(str) {
+  if (str === 'generous') {
+    return 'Thank you so much.';
+  } else if (str === 'thanks for everything') {
+    return 'Bye.';
+  } else {
+    return 'Thank you.';
+  }
+}
