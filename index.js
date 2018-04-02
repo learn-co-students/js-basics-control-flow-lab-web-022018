954ff8d2773eab628cb92f6d83216bbd3d4bbe70
zafinar <zafinar@gmail.com>
2018-04-02 10:44:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400){
+  response = 'This one is on me!'}
+  else if (feet > 2500){
+  response = 'No can do.'}
+  else if (feet > 2000){
+  response = 'I will gladly take your thirty bucks.'}
+
+  return response
+
+
+
+}
+
+function ternaryCheckCity(city){
+if (city === "NYC"){
+ response = "Ok, sounds good."
+}
+else {
+  response = "No go."
+}
+return response
+
+}
+
+function switchOnCharmFromTip(tip) {
+switch(tip){
+case 'generous':
+  return 'Thank you so much.'
+  break
+case 'not as generous':
+  return 'Thank you.'
+  break
+default:
+  return 'Bye.'
+  break
+}
+
+}
