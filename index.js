3c08f1956a1629695e450339a4c9e7b3634c139c
bafab <bafa@tutanota.de>
2018-04-06 17:52:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
-    return 'This one is on me!';
-  } else if (1999 < feet && feet < 2500) {
-    return 'I will gladly take your thirty bucks.';
+    return 'This one is on me!'
+  } else if (1999 < feet && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
   } else {
-    return 'No can do.';
+    return 'No can do.'
   }
 }
 
 function ternaryCheckCity(city) {
-  if (city === 'NYC') {
-    return 'Ok, sounds good.';
-  } else {
-    return 'No go.';
-  }
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.'
 }
 
 function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
-    return "Thank you so much.";
-    break;
+    return 'Thank you so much.'
     case 'not as generous':
-    return "Thank you.";
-    break;
+    return 'Thank you.'
     default:
-    return "Bye.";
+    return 'Bye.'
   }
 }
